179a275e3ebfa0306fc84cfc22d41ce0808588c2
Semenciuc, Cosmin <cosmin.semenciuc@garmin.com>
2026-04-06 11:20:20 +0300

    Add basic project structure and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,24 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.google.ksp)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
     namespace = "com.example.androidcomposeprojecttemplate"
     compileSdk {
-        version = release(36) {
+        version = release(libs.versions.compileSdk.get().toInt()) {
             minorApiLevel = 1
         }
     }
 
     defaultConfig {
         applicationId = "com.example.androidcomposeprojecttemplate"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -31,8 +35,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
@@ -42,17 +46,50 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(platform(libs.coil.bom))
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+    implementation(libs.coil.network)
+
+    implementation(libs.data.store)
+
+    // DETEKT
+    detektPlugins(libs.detekt.formatting)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.navigation)
+
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.kotlinx.immutableCollections)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.navigation.compose)
+
+    implementation(platform(libs.retrofit.bom))
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(platform(libs.okhttp3.bom))
+    implementation(libs.okhttp3)
+
+    implementation(libs.androidx.paging.common)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.paging.runtime)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    androidTestImplementation(libs.androidx.room.testing)
 }
--- app/src/main/kotlin/com/example/androidcomposeprojecttemplate/app/MainActivity.kt
+++ app/src/main/kotlin/com/example/androidcomposeprojecttemplate/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.androidcomposeprojecttemplate
+package com.example.androidcomposeprojecttemplate.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
